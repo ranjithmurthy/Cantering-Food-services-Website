d0759a5a51b72122e72c9d95e923f77f4dac1205
Ranjith Murthy <ranjith.murthy@gmail.com>
2017-04-08 22:17:15 +0200

    added ML projects to solution

---
+++ Dragon-master/NLPToolkit/Sentenizer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using java.io;
+using opennlp.tools.sentdetect;
+
+namespace NLPToolkit
+{
+    /// <summary>
+    ///     http://opennlp.apache.org/documentation/manual/opennlp.html#tools.sentdetect.detection.api
+    /// </summary>
+    public static class Sentenizer
+    {
+        private static readonly FileInputStream ModelIn;
+        private static readonly SentenceModel Model;
+
+        static Sentenizer()
+        {
+            // var modelFile = ConfigurationManager.AppSettings["ModelSentenizer"] ?? string.Empty;
+
+            var modelFile = AppDomain.CurrentDomain.BaseDirectory + "Repository\\en-sent.bin";
+            ModelIn = new FileInputStream(modelFile);
+            Model = new SentenceModel(ModelIn);
+        }
+
+        /// <summary>
+        ///     Split the input content to individual words
+        /// </summary>
+        /// <param name="contents">Content to split into words</param>
+        /// <returns></returns>
+        public static IEnumerable<string> ExtractSentences(string contents)
+        {
+            var sentenizer = new SentenceDetectorME(Model);
+            var sentences = sentenizer.sentDetect(contents);
+
+            return sentences;
+        }
+    }
+}
